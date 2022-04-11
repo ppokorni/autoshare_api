e9e64aaa349cfe68f09ccfde076100d9d2959dda
Patrik Pokorni <patrik@exevio.com>
2022-04-11 12:43:40 +0200

    Define user's acessors & mutators

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -25,4 +26,25 @@ class User extends Authenticatable {
     protected $hidden = [
         'password',
     ];
+
+    //get formatted attributes
+    protected $appends = [
+        'renter_avg_rating',
+        'rentee_avg_rating',
+    ];
+
+    //Set the user's date of birth to start of day.
+    public function setDateOfBirthAttribute($value) {
+        $this->attributes['date_of_birth'] = Carbon::parse($value)->startOfDay();
+    }
+
+    //Format user's renter rating.
+    public function getRenterAvgRatingAttribute($value) {
+        return number_format($value, 2);
+    }
+
+    //Format user's rentee rating.
+    public function getRenteeAvgRatingAttribute($value) {
+        return number_format($value, 2);
+    }
 }
