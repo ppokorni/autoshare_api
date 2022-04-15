a7b62400344ec7f15abece522f8d48af84fab048
Patrik Pokorni <patrikpokorni@gmail.com>
2022-04-15 17:46:34 +0200

    Create availability migration and model

---
+++ database/migrations/2022_04_15_130255_create_availabilities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvailabilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availabilities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('vehicle_id');
+            $table->dateTime('start_time');
+            $table->dateTime('end_time');
+            $table->float('latitude');
+            $table->float('longitude');
+            $table->timestamps();
+
+            // Foreign key constraints (when deleting a vehicle, delete all its availabilities)
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('availabilities');
+    }
+}
