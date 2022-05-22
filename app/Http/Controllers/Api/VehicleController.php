3887e91f7de67847a46cea25243ed55fa90a881b
Darijan Jelušić <darijan.jelusic@gmail.com>
2022-05-22 13:14:20 +0200

    Create endpoint, search availabilities by location

--- app/Http/Controllers/Api/VehicleController.php
+++ app/Http/Controllers/Api/VehicleController.php
@@ -92,4 +92,47 @@ class VehicleController extends Controller {
             return response()->json(['error' => $e->errors()], 400);
         }
     }
+
+
+    // Function searches vehicles based on availability, location and features
+    public function search(Request $request) {
+//        $vehicles = DB::table('vehicles')
+//            ->join('vehicle_features', 'vehicles.vehicle_id', '=', 'vehicle_features.vehicle_id')
+//            ->join('availabilities', 'vehicles.vehicle_id', 'availabilities.vehicle_id')
+//            ->get();
+        $latpoint = 45;
+        $longpoint = 15;
+        $radius = 200;
+
+        // Query taken from http://www.plumislandmedia.net/mysql/haversine-mysql-nearest-loc/
+        $vehicles = DB::select(DB::raw("
+                    SELECT *
+                      FROM (
+                     SELECT a.vehicle_id,
+                            a.start_time, a.end_time,
+                            a.latitude, a.longitude,
+                            p.radius,
+                            p.distance_unit
+                                     * DEGREES(ACOS(LEAST(1.0, COS(RADIANS(p.latpoint))
+                                     * COS(RADIANS(a.latitude))
+                                     * COS(RADIANS(p.longpoint - a.longitude))
+                                     + SIN(RADIANS(p.latpoint))
+                                     * SIN(RADIANS(a.latitude))))) AS distance
+                      FROM availabilities AS a
+                      JOIN (   /* parametri upita */
+                            SELECT  $latpoint  AS latpoint,  $longpoint AS longpoint,
+                                    $radius AS radius,      111.045 AS distance_unit
+                        ) AS p ON 1=1
+                      WHERE a.latitude
+                         BETWEEN p.latpoint  - (p.radius / p.distance_unit)
+                             AND p.latpoint  + (p.radius / p.distance_unit)
+                        AND a.longitude
+                         BETWEEN p.longpoint - (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+                             AND p.longpoint + (p.radius / (p.distance_unit * COS(RADIANS(p.latpoint))))
+                     ) AS d
+                     WHERE distance <= radius
+                     ORDER BY distance
+                     "));
+        return response()->json($vehicles, 200);
+    }
 }
