a3c8d72c4f1e0a17b882801f73def43a1c0137b1
Darijan Jelušić <darijan.jelusic@gmail.com>
2022-05-30 19:53:30 +0200

    Create image service

---
+++ app/Services/ImageService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+
+class ImageService {
+
+    // Stores base64 encoded image to specified path with specified filename
+    public function storeImage($encoded_image, $path, $id) {
+        // Creates directory if necessary
+        if (!file_exists($path)) {
+            mkdir($path, 0755, true);
+        }
+        if (file_exists("$path/$id.png")) {
+            unlink("$path/$id.png");
+        }
+
+        $data = base64_decode($encoded_image);
+        $image = imagecreatefromstring($data);
+        imagepng($image, "$path/$id.png");
+    }
+
+    // Encodes image to base64
+    public function encodeImage($image): string {
+        $data = file_get_contents($image);
+        return base64_encode($data);
+    }
+}
